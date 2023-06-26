b30ff46a0841d69dd370122cfc66f6c584d2f22c
Amanda Letícia <102308691+amandaalvees@users.noreply.github.com>
2023-06-26 20:40:40 -0300

    Atividade 1 - 26/06 - Tquinta
    Atividade de fixação - Start

---
+++ atividade 1/index.js
@@ -0,0 +1,37 @@
+NUMBER
+let num = 2+1
+console.log(num);
+let num1 = 3-1
+console.log(num1);
+
+STRING
+let nome = 'Amanda'
+console.log(nome);
+let animal = 'Gato'
+console.log(animal);
+
+BOOLEAN 
+let verdade = true;
+console.log('Hoje está sol?')
+console.log(verdade); 
+let falso = false; 
+console.log('Hoje está chovendo?')
+console.log(falso);
+
+ARRAY
+let mercado = ["Pão", "feijão", "arroz", "carne"];
+console.log(mercado)
+let instrumentos = ["Violão", "Teclado","Bateria"];
+console.log(instrumentos)
+
+Object
+let celular  = {cor: "branco", modelo: "Iphone 13"};
+console.log(celular)
+let moto = {cor:"Azul", modelo: "naked", ano: "2021"}
+console.log(moto)
+
+UNDERFINED/NULL
+var semvalor;
+console.log("valor_nao_definido: " + semvalor);
+var valornull = null;
+console.log("valor_vazio: " + valornull);
